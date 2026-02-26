58ca589bbd4bf23af9fcbed58fba6faa1c18a52f
beaandres <bandres006@ikasle.ehu.eus>
2026-02-26 11:26:56 +0100

    #12 AsText and Parse from Column definition and tests

--- OurTests/ColumnDefinitionTests.cs
+++ OurTests/ColumnDefinitionTests.cs
@@ -19,6 +19,31 @@ namespace OurTests
             //comprobamos el formato
             Assert.Equal("String->columnaPrueba", resultado);
         }
-        
+        [Fact]
+        public void TestParserValido()
+        {
+            //prueba válida
+            String prueba = "String->Name";
+
+            ColumnDefinition column = ColumnDefinition.Parse(prueba);
+
+            Assert.Equal(ColumnDefinition.DataType.String, column.Type);
+
+            Assert.Equal("Name", column.Name);
+        }
+
+        [Fact]
+        public void TestParserDelimEncode()
+        {
+            //delimitador codificado (encode) 
+            ColumnDefinition column = new ColumnDefinition(ColumnDefinition.DataType.String, "Test->Columns");
+            
+            String text = column.AsText();
+
+            ColumnDefinition parsedColumn = ColumnDefinition.Parse(text);
+
+            Assert.Equal(column.Type, parsedColumn.Type);
+            Assert.Equal(column.Name, parsedColumn.Name);
+        }
     }
 }
