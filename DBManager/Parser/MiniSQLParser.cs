67e6904b318b169b4f765dcac78dfeb4dc23b360
JORGE ORTIZ DE MENDIVIL VICENTE <jortizdemendiv001@ikasle.ehu.eus>
2026-03-17 03:24:32 +0100

    #25 test setup fixed

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -1,4 +1,5 @@
 using DbManager.Parser;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
@@ -10,35 +11,35 @@ namespace DbManager
         {
             //TODO DEADLINE 2
             const string selectPattern = @"^SELECT\s+([a-zA-Z0-9]+(?:,[a-zA-Z0-9]+)*)\s+FROM\s+([a-zA-Z0-9]+)(\s+WHERE\s+([a-zA-Z0-9]+(>|<|=)'((-[0-9]+(\.[0-9]+)?)|([0-9]+(\.[0-9]+)?)|([a-zA-Z0-9]+(?:\s[a-zA-Z0-9]+)*))'))?$";
-            
-            
+
+
             const string dropTablePattern = @"^DROP\s+TABLE\s+([a-zA-Z0-9]+)$";
-          
+
 
             //LEIRE --> #16
             const string insertPattern = @"INSERT\s+INTO\s+(\w+)\s+VALUES\s*\(([^)]+)\)";
 
-            
+
             //Note: The parsing of CREATE TABLE should accept empty columns "()"
             //And then, an execution error should be given if a CreateTable without columns is executed
             const string createTablePattern = @"CREATE\s+TABLE\s+([a-zA-Z][a-zA-Z0-9]*)\s*\(\s*(.*?)\s*\)";
-            
+
             const string updateTablePattern = null;
-            
-            const string deletePattern = @"^DELETE\s+FROM\s+([a-zA-Z0-9]+)\s+WHERE\s+([a-zA-Z0-9]+)(<|=|>)'([^']*)'$"; 
-            
+
+            const string deletePattern = @"^DELETE\s+FROM\s+([a-zA-Z0-9]+)\s+WHERE\s+([a-zA-Z0-9]+)(<|=|>)'([^']*)'$";
+
 
             //TODO DEADLINE 4
             const string createSecurityProfilePattern = null;
-            
+
             const string dropSecurityProfilePattern = null;
-            
+
             const string grantPattern = null;
-            
+
             const string revokePattern = null;
-            
+
             const string addUserPattern = null;
-            
+
             const string deleteUserPattern = null;
 
 
@@ -47,6 +48,14 @@ namespace DbManager
             //For example, if the query is a "SELECT ...", there should be a match with selectPattern. We would create and return an instance of Select
             //initialized with the table name, the columns, and (possibly) an instance of Condition.
             //If there is no match, it means there is a syntax error. We will return null.
+
+            //droptable
+            Match dropMatch = Regex.Match(miniSQLQuery, dropTablePattern);
+            if (dropMatch.Success)
+            {
+                string tableName = dropMatch.Groups[1].Value;
+                return new DropTable(tableName);
+            }
 
             //Select
             Match matchSelect = Regex.Match(miniSQLQuery, selectPattern);
@@ -67,27 +76,15 @@ namespace DbManager
                 //Nombre de las columnas
                 List<string> columns = CommaSeparatedNames(columnas);
 
-  
+
                 Condition condition = new Condition(columna, simbolo, comparando);
                 Select select = new Select(table, columns, condition);
 
                 return select;
             }
-         
-            {
-                return null;
-            }
 
-            {
-                return null;
-            }
-
-            {
-                return null;
-            }
-
-                //Insert
-                Match matchInsert = Regex.Match(miniSQLQuery, insertPattern);
+            //Insert
+            Match matchInsert = Regex.Match(miniSQLQuery, insertPattern);
             if (matchInsert.Success)
             {
                 string table = matchInsert.Groups[1].Value;
@@ -96,8 +93,9 @@ namespace DbManager
                 string newPattern = @"'[^']+'";
                 MatchCollection matchCollectionComillas = Regex.Matches(valores, newPattern);
 
-                //Contar cuantas comas hay
-                int commaCount = valores.Split(',').Length - 1;
+                //Contar cuantas comas hay y sustituir por #
+                string sinComillas = Regex.Replace(valores, @"'[^']*'", "#");
+                int commaCount = sinComillas.Split(',').Length - 1;
 
                 //Ver si hay los mismos valores que comas +1
                 if (matchCollectionComillas.Count == 0 || matchCollectionComillas.Count != commaCount + 1)
@@ -117,10 +115,10 @@ namespace DbManager
                 return new Insert(table, values);
             }
 
-           
-            
+
+
             //create table
-            Match createMatch = Regex.Match(miniSQLQuery,createTablePattern);
+            Match createMatch = Regex.Match(miniSQLQuery, createTablePattern);
 
             //CREATE TABLE válido
             if (createMatch.Success)
@@ -172,25 +170,25 @@ namespace DbManager
                         }
                     }
                 }
-                    return new CreateTable(nombreTabla, crearColumnas);
+                return new CreateTable(nombreTabla, crearColumnas);
             }
             //TODO DEADLINE 4
             //Do the same for the security queries (CREATE SECURITY PROFILE, ...)
 
             return null;
-           
+
         }
 
         static List<string> CommaSeparatedNames(string text)
         {
             string[] textParts = text.Split(",", System.StringSplitOptions.RemoveEmptyEntries);
             List<string> commaSeparator = new List<string>();
-            for(int i=0; i < textParts.Length; i++)
+            for (int i = 0; i < textParts.Length; i++)
             {
                 commaSeparator.Add(textParts[i]);
             }
             return commaSeparator;
         }
-        
+
     }
 }
