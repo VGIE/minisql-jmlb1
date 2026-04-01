177f783025962f85b376473c72d8f082cc2d3787
maria <maria.ortizdepinedo@outlook.com>
2026-04-01 17:18:40 +0200

    #31 Add User constructor with password encryption

--- DBManager/Security/User.cs
+++ DBManager/Security/User.cs
@@ -13,7 +13,8 @@ namespace DbManager.Security
         public User(string username, string password)
         {
             //TODO DEADLINE 5: Initialize the member variables. We must encrypt the password
-            
+            Username = username.Trim();
+            EncryptedPassword = Encryption.Encrypt(password);
         }
 
         public User() { }
