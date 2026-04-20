66535d4af48bb50f6c86795dad48164d842a831d
leeiree <leirejimeneza@gmail.com>
2026-04-20 16:06:27 +0200

    #23 correcting spaces

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -14,7 +14,6 @@ namespace DbManager
             //TODO DEADLINE 2
             const string selectPattern = @"^SELECT\s+([a-zA-Z0-9]+(?:,[a-zA-Z0-9]+)*)\s+FROM\s+([a-zA-Z0-9]+)(\s+WHERE\s+([a-zA-Z0-9]+(>|<|=)'((-[0-9]+(\.[0-9]+)?)|([0-9]+(\.[0-9]+)?)|([a-zA-Z0-9]+(?:\s[a-zA-Z0-9]+)*))'))?$";
 
-
             const string dropTablePattern = @"^DROP\s+TABLE\s+([a-zA-Z0-9]+)$";
 
 
@@ -24,7 +23,7 @@ namespace DbManager
 
             //Note: The parsing of CREATE TABLE should accept empty columns "()"
             //And then, an execution error should be given if a CreateTable without columns is executed
-            const string createTablePattern = @"CREATE\s+TABLE\s+([a-zA-Z][a-zA-Z0-9]*)\s*\((.*)\)";
+            const string createTablePattern = @"^CREATE\s+TABLE\s+([a-zA-Z][a-zA-Z0-9]*)\s*\(([a-zA-Z][a-zA-Z0-9]*\s+(?:INT|TEXT)(?:,[a-zA-Z][a-zA-Z0-9]*\s+(?:INT|TEXT))*)\)$";
             //LEIRE --> #26
             const string updateTablePattern = @"UPDATE\s+(\w+)\s+SET\s+(.+?)(?:\s+WHERE\s+(\w+)\s*([=<>])\s*('[^']*'))?$";
 
