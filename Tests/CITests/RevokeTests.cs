280e377181ea87e7e458cbb6a97787db3e9dd315
JORGE ORTIZ DE MENDIVIL VICENTE <jortizdemendiv001@ikasle.ehu.eus>
2026-05-11 21:46:54 +0200

    #52 revoke pattern finished, tests added

--- Tests/CITests/RevokeTests.cs
+++ Tests/CITests/RevokeTests.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -134,5 +134,110 @@ namespace SecurityParsingTests
             query = MiniSQLParser.Parse("REVOKE UPDATE ON Table TO User") as Revoke;
             Assert.NotNull(query);
         }
+
+        //Test adicionales
+
+        [Fact]
+        public void InvalidTableStartsWithNumber()
+        {
+            //Tabla no puede empezar por un numero
+            Revoke query = MiniSQLParser.Parse("REVOKE SELECT ON 1Table TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON 123 TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table1 TO User") as Revoke;
+            Assert.NotNull(query); //si acabar
+        }
+
+        [Fact]
+        public void InvalidUserStartsWithNumber()
+        {
+            //Usuario no puede empezar por numero 
+            Revoke query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO 1User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO 99") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User99") as Revoke;
+            Assert.NotNull(query); //si acabar
+        }
+
+        [Fact]
+        public void InvalidTableChars()
+        {
+            //Tabla con caracteres correctos
+            Revoke query = MiniSQLParser.Parse("REVOKE SELECT ON Table-1 TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table_Name TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table.Name TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User") as Revoke;
+            Assert.NotNull(query);
+        }
+
+        [Fact]
+        public void InvalidExtraTokens()
+        {
+            //Debe terminar por User
+            Revoke query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User ExtraToken") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User;") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User") as Revoke;
+            Assert.NotNull(query);
+        }
+
+        [Fact]
+        public void InvalidKeywords()
+        {
+            //Palabras clave inválidas
+            Revoke query = MiniSQLParser.Parse("GRANT SELECT ON Table TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT IN Table TO User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table FROM User") as Revoke;
+            Assert.Null(query);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON Table TO User") as Revoke;
+            Assert.NotNull(query);
+        }
+
+        [Fact]
+        public void ValidAlphanumericNames()
+        {
+            //Tabla y usuario correctos
+            Revoke query = MiniSQLParser.Parse("REVOKE SELECT ON Table1 TO User1") as Revoke;
+            Assert.Equal("SELECT", query.PrivilegeName);
+            Assert.Equal("Table1", query.TableName);
+            Assert.Equal("User1", query.ProfileName);
+        }
+
+        [Fact]
+        public void ValidPrivilegesParsed()
+        {
+            //Privilegios válidos
+            Revoke query = MiniSQLParser.Parse("REVOKE DELETE ON T TO U") as Revoke;
+            Assert.Equal("DELETE", query.PrivilegeName);
+
+            query = MiniSQLParser.Parse("REVOKE INSERT ON T TO U") as Revoke;
+            Assert.Equal("INSERT", query.PrivilegeName);
+
+            query = MiniSQLParser.Parse("REVOKE SELECT ON T TO U") as Revoke;
+            Assert.Equal("SELECT", query.PrivilegeName);
+
+            query = MiniSQLParser.Parse("REVOKE UPDATE ON T TO U") as Revoke;
+            Assert.Equal("UPDATE", query.PrivilegeName);
+        }
     }
-}*/
+}
